8a6bc3e4d8dd1174223e1e13b87307b384f39fd4
Quirin Hirmer <quirin.hirmer@gmail.com>
2018-07-23 00:59:01 +0200

    Application config
    This file should be included in every .cpp file

---
+++ src/app_config.h
@@ -0,0 +1,11 @@
+#pragma once
+
+// This file configures certain hard-coded application behaviour.
+
+#include <string>
+#include <string_view>
+using namespace std::string_literals;
+using namespace std::string_view_literals;
+
+constexpr const auto CONFIG_PATH = "./config/";
+constexpr const auto CONFIG_SPACES_PER_INDENT_LEVEL = 4;
