c52fca7343c144cf08f59e35e8e41f71b4a36300
Quirin Hirmer <quirin.hirmer@gmail.com>
2018-10-18 03:55:10 +0200

    This is no longer true

--- src/logging/logging.h
+++ src/logging/logging.h
@@ -2,9 +2,6 @@
 
 #include "spdlog/spdlog.h"
 
-// Currently formatting in log messages using fmt's way (ie. {}) causes an internal compiler error in the project, so
-// use string concatenation and the type conversion functions in type_conversion.h
-
 namespace logging {
     using log = std::shared_ptr<spdlog::logger>;
 
