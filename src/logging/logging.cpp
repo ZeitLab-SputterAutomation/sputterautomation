e399fc8f5b1d0fc5077de6edc055998e9c8a8c90
Quirin Hirmer <quirin.hirmer@gmail.com>
2018-12-13 04:19:05 +0100

    Changed the main logger to include multiple sinks
    The file sink however is disabled during development, enable it by uncommenting line 10

--- src/logging/logging.cpp
+++ src/logging/logging.cpp
@@ -1,14 +1,19 @@
 #include "logging.h"
 
 #include "spdlog/sinks/stdout_color_sinks.h"
+#include "spdlog/sinks/basic_file_sink.h"
 
 namespace logging {
     void init() {
-        auto main_logger = spdlog::stdout_color_mt("main");
+        std::vector<spdlog::sink_ptr> sinks;
+        sinks.push_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
+        //sinks.push_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("log.txt"));
+
+        auto main_logger = std::make_shared<spdlog::logger>("main", std::begin(sinks), std::end(sinks));
         main_logger->set_level(spdlog::level::trace);
+
+        spdlog::register_logger(main_logger);
     }
 
-    log get_log(const std::string &name) {
-        return spdlog::get(name);
-    }
+    log get_log(const std::string &name) { return spdlog::get(name); }
 }  // namespace logging
